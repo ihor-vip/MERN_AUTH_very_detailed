1ccb6367b6276bb349c01536641c25409be01372
Ihor Ploskodnyak <vip_anonymous@Ihors-Mac-mini.local>
2025-03-12 14:52:44 -0500

    database setup - mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port: ", PORT);
 });
